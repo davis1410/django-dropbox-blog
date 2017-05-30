7d0ca22d9a07d8484927e9e07456f7972f5246b6
Brennan Davis <brennan.davis1410@gmail.com>
2017-05-29 23:01:56 -0600

    added Post model

--- models.py
+++ models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.conf import settings
 
-# Create your models here.
+from django.utils.timezone import now
+
+import dropbox
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    publish = models.BooleanField("Published", default=False)
+    date_published = models.DateField("Date Published", default=now)
+    post_filename = models.CharField("Post Filename", max_length=200)
+
+    def save(self, *args, **kwargs):
+        if self.slug is None:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)

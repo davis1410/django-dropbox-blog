ae04e070656656b67dc5740558c122505a07d036
Brennan Davis <brennan.davis1410@gmail.com>
2017-06-05 15:16:34 -0600

    pulling post content from markdown files in dropbox

--- models.py
+++ models.py
@@ -13,7 +13,28 @@ class Post(models.Model):
     date_published = models.DateField("Date Published", default=now)
     post_filename = models.CharField("Post Filename", max_length=200)
 
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    def __str__(self):
+        return self.title
+
     def save(self, *args, **kwargs):
         if self.slug is None:
             self.slug = slugify(self.title)
         super(Post, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return "/%s" % self.slug
+
+    def get_post_content(self):
+        dbx = dropbox.Dropbox(settings.DROPBOX_TOKEN)
+
+        directory = self.date_published.strftime("%Y")
+        filename = self.post_filename
+
+        meta, file = dbx.files_download('/%s/%s.md' % (directory, filename))
+        file_content = file.content
+
+        return file_content
--- views.py
+++ views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def blog_landing(request):
+    context = {}
+
+    published_posts = Post.objects.filter(publish=True).order_by("-date_published")
+    context['posts'] = published_posts
+
+    return render(request, "blog_landing.html", context)
